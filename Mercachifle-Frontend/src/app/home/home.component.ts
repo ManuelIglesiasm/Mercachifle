b1a5c1a68f666271779289136663873f9f0165c8
Elias.V <eliasvillamayor2@gmail.com>
2022-10-25 10:21:23 -0300

    agregue el inicio de home

--- Mercachifle-Frontend/src/app/home/home.component.ts
+++ Mercachifle-Frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+  pasarARegister(){
+    this.router.navigate(['/register'])
+  }
+  pasarALogin(){
+    this.router.navigate(['/login'])
+  }
 
 }
+
+
+
